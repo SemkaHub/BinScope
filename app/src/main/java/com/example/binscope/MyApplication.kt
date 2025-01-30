11f0be14533aaf7ed35f59c5b6e7a0c23bee401f
Semyon <xuevbli@gmail.com>
2025-01-31 00:24:52 +1200

    Implement basic main screen

---
+++ app/src/main/java/com/example/binscope/MyApplication.kt
@@ -0,0 +1,6 @@
+package com.example.binscope
+
+import android.app.Application
+
+class MyApplication: Application() {
+}
---
+++ app/src/main/java/com/example/binscope/presentation/ui/fragment/DetailsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.binscope.presentation.ui.fragment
+
+import androidx.fragment.app.Fragment
+
+class DetailsFragment : Fragment() {
+}
